fda64c3881d6c972187c8f24b188a410693d204e
Luis Angel Prado Postigo <lpradopostigo@gmail.com>
2021-03-22 15:59:40 -0500

    cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,21 @@
 #include "image_cipher.hpp"
 #include <iostream>
 
+// USAGE: image-cipher [input] [key] [output]
 int main(int argc, char* argv[]) {
-  // se carga la imagen y se crea una vector de pixeles con la imagen cargada
+  // Load img and create a std vector from it
   auto img{load_image(argv[1])};
-
   auto img_vector{create_vector_from_image(img)};
 
-  // se carga la llave y se genera el vector a partir de esta
+  // Load key and create a std vector from it
   auto key_vector{create_vector_from_key(argv[2], img.width * img.height * 3)};
 
-  // se aplica la operacion xor entre ambos vectores
+  // xor operation between vectors
   for (unsigned int i{0}; i < img_vector.size(); ++i) {
     img_vector[i] ^= key_vector[i];
   }
 
-  // se guarda la imagen
+  // Save img and free memory
   vector_to_raw_data(img_vector, img);
   write_image(argv[3], img);
   stbi_image_free(img.data);
